37a409fa982643d001f37feb6480190a76eceaf5
iancleary <hello@iancleary.me>
2022-11-14 14:17:43 +0000

    refactor Session, engine, and encoder

--- app/database/tables.py
+++ app/database/tables.py
@@ -1,19 +1,7 @@
-import os
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 import app.models.projects as projects
-
-
-DATABASE = os.getenv("DATABASE", "/data/data.db")
-engine = create_engine(f"sqlite:///{DATABASE}", echo=True, future=True)
-Session = sessionmaker(engine)
+from app.database import ENGINE
 
 
 def create_tables() -> None:
-    print(engine)
-    projects.Base.metadata.create_all(engine)
-
-def get_session() -> None:
-    return Session
+    print(ENGINE)
+    projects.Base.metadata.create_all(ENGINE)
