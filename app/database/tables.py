5cecb522f1a68d3351c1ad59ebf7ea20647861b7
iancleary <hello@iancleary.me>
2022-11-14 14:54:48 +0000

    Start to move project router

--- app/database/tables.py
+++ app/database/tables.py
@@ -1,7 +1,7 @@
+import app.database as database
 import app.models.projects as projects
-from app.database import ENGINE
 
 
 def create_tables() -> None:
-    print(ENGINE)
-    projects.Base.metadata.create_all(ENGINE)
+    print(database.ENGINE)
+    projects.Base.metadata.create_all(database.ENGINE)
