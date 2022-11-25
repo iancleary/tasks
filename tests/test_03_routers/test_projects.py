587355bdf0861fb7ef15cea6204aa8bf91a721b6
iancleary <hello@iancleary.me>
2022-11-25 19:04:28 +0000

    Initial Project Testing

--- tests/test_03_routers/test_projects.py
+++ tests/test_03_routers/test_projects.py
@@ -5,6 +5,13 @@ from app.main import app
 client = TestClient(app)
 
 
-# def test_add_project() -> None:
-#     response = client.put("/project", json={"name": "Test Project"})
-#     assert response.status_code == 200
+def test_create_project() -> None:
+    response = client.post("/project", json={"name": "Test Project"})
+    assert response.status_code == 200
+
+
+def test_patch_project() -> None:
+    response = client.patch(
+        "/project/1", json={"name": "Test Project", "active": "True"}
+    )
+    assert response.status_code == 200
