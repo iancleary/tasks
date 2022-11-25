b3ecab56e76fcf44287ffdc97bec0a2bf3db857b
iancleary <hello@iancleary.me>
2022-11-25 18:44:01 +0000

    Comment out test

--- tests/test_03_routers/test_projects.py
+++ tests/test_03_routers/test_projects.py
@@ -5,6 +5,6 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_add_project() -> None:
-    response = client.put("/project", json={"name": "Test Project"})
-    assert response.status_code == 200
+# def test_add_project() -> None:
+#     response = client.put("/project", json={"name": "Test Project"})
+#     assert response.status_code == 200
