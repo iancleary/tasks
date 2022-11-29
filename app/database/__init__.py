ba2681367d7cffb8298f3c96538de9fe83c84d9b
iancleary <hello@iancleary.me>
2022-11-28 18:48:46 -0700

    Update environments, vscode debug configurations, default python interpreter

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -3,6 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE = os.getenv("DATABASE", "/data/data.db")
+DATABASE = os.getenv("DATABASE")
 ENGINE = create_engine(f"sqlite:///{DATABASE}", echo=True, future=True)
 SESSION = sessionmaker(ENGINE)
