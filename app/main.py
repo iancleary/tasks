2ba72ad67f4f1cdc06978e7fc6c14cc3db9f6b4b
iancleary <hello@iancleary.me>
2022-12-08 21:20:55 +0000

    Update ports to work in docker

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
 
@@ -21,4 +23,7 @@ def read_root() -> dict[str, str]:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    log_level = os.getenv("LOG_LEVEL", "info")
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
