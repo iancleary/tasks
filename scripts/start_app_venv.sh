81e17019d66bd979e15e932550d71ed42f7ada78
iancleary <hello@iancleary.me>
2022-12-08 21:05:44 +0000

    Update venv run script for items

--- scripts/start_app_venv.sh
+++ scripts/start_app_venv.sh
@@ -5,4 +5,9 @@ set -x
 export HOST=0.0.0.0
 export PORT=8080
 export DATABASE=data/dev.db
-python3 app/main.py
+# assuming the script is called from `make run`
+# it will be run from ../ such that main.py
+# relatively is app.main
+# which ensures the import statements are consistent
+# between make run and docker(-compose)
+python3 -m app.main
