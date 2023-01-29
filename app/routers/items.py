1be5975629ada838c9d76f56b31473ebbb4004a6
iancleary <dark.year4987@fastmail.com>
2023-01-29 16:06:53 -0700

    Add get_pinned_items, need test

--- app/routers/items.py
+++ app/routers/items.py
@@ -62,7 +62,11 @@ def get_open_items(db: Session = Depends(get_db)) -> List[PydanticItem]:
 @router.get("/items/pinned")
 def get_pinned_items(db: Session = Depends(get_db)) -> List[PydanticItem]:
     items = db.query(Item).filter(
-        and_(Item.status == Status.OPEN, Item.status == Pinned.YES, Item.active == Active.YES)
+        and_(
+            Item.status == Status.OPEN,
+            Item.status == Pinned.YES,
+            Item.active == Active.YES,
+        )
     )
 
     json_compatible_return_data = [jsonable_encoder(x) for x in items]
