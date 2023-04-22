7eadec3155471e41ae5e1165d5963dc755475070
iancleary <github@iancleary.me>
2023-04-22 15:12:51 -0700

    Update typing on get_item_by_id function

--- app/database/items.py
+++ app/database/items.py
@@ -1,8 +1,11 @@
+from typing import Union
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from app.models.items import Item
 
 
-def get_item_by_id(db: Session, item_id: int):
+def get_item_by_id(db: Session, item_id: Union[int, str]) -> Union[Item, None]:
+    if isinstance(item_id, str):
+        item_id = int(item_id)
     item = db.execute(select(Item).filter(Item.id == item_id)).scalar_one_or_none()
     return item
