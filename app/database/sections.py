0ff902e485c964d1b0a61db90c9ce9dd1821f114
iancleary <github@iancleary.me>
2023-04-25 16:46:38 -0700

    Sections database functions, first pass with linting

--- app/database/sections.py
+++ app/database/sections.py
@@ -1,105 +1,128 @@
+from typing import List
 from typing import Union
 
+from sqlalchemy import insert
 from sqlalchemy import select
 from sqlalchemy import update
+from sqlalchemy.orm import Session
 
-from app.database.core import get_session
 from app.models.sections import SectionObject
 from app.models.utils.columns import StrListConverter
 
 
 class SectionNotFoundExeption(Exception):
-    def __init__(self, section_list_id: str):
-        self.section_list_id = section_list_id
+    def __init__(self, section_id: str):
+        self.section_id = section_id
 
     def __str__(self) -> str:
-        return f"Section list with id {self.section_list_id} not found."
+        return f"Section with id {self.section_id} not found."
 
 
-def create_new_section_list_object_in_database(name: str, list_id: int) -> None:
-    section_list_obj = SectionObject(name=name, list_id=list_id)
-    database_session = get_session()
-    with database_session.begin() as session:
-        session.add(section_list_obj)
+def create_new_section_object_in_database(
+    *, session: Session, name: str  # keyword arguments only
+) -> int:
+    new_section_obj_iterator = session.execute(
+        insert(SectionObject).returning(SectionObject.id), {"name": name}
+    )
+    new_section_obj_id = new_section_obj_iterator.scalar_one()
+    return new_section_obj_id
 
 
-def select_section_list_obj_by_id(id: int) -> Union[SectionObject, None]:
-    database_session = get_session()
-    with database_session.begin() as session:
-        obj = session.execute(
-            select(SectionObject).where(SectionObject.id == id)
-        ).scalar_one_or_none()
-        return obj
+def select_section_object_by_id(
+    *, session: Session, section_id: int  # keyword arguments only
+) -> Union[SectionObject, None]:
+    obj = session.execute(
+        select(SectionObject).where(SectionObject.id == section_id)
+    ).scalar_one_or_none()
+    return obj
 
 
-def update_section_list_object_in_database(section_list_object: SectionObject) -> None:
-    database_session = get_session()
+def select_all_section_objects(
+    *, session: Session  # keyword arguments only
+) -> Union[List[SectionObject], None]:
+    iterator_result = session.scalars(select(SectionObject))
+    # all_rows = [x for x in iterator_result]
+    # all_section_objects = [x[0] for x in all_rows]
+    # considalte the above two lines into one line (faster)
+    all_section_objects = [x for x in iterator_result]
+    return all_section_objects
+
+
+def update_section_object_in_database(
+    *, session: Session, section_object: SectionObject
+) -> None:
     stmt = update(SectionObject)
     stmt = stmt.values(
         {
-            "id": section_list_object.id,
-            "name": section_list_object.name,
-            "items": section_list_object.items,
-            "list_id": section_list_object.list_id,
+            "id": section_object.id,
+            "name": section_object.name,
+            "items": section_object.items,
         }
     )
-    stmt = stmt.where(SectionObject.id == section_list_object.id)
-    with database_session.begin() as session:
-        session.execute(stmt)
+    stmt = stmt.where(SectionObject.id == section_object.id)
+    session.execute(stmt)
 
 
-def delete_section_list_object_from_database(section_list_id: int) -> None:
-    section_list_obj = select_section_list_obj_by_id(section_list_id)
-    database_session = get_session()
-    with database_session.begin() as session:
-        session.delete(section_list_obj)
+def delete_section_object_from_database(*, session: Session, section_id: int) -> None:
+    section_object = select_section_object_by_id(session=session, section_id=section_id)
+
+    if section_object is None:
+        raise SectionNotFoundExeption(section_id=str(section_id))
+
+    session.delete(section_object)
 
 
-def insert_item_id_to_section_list_object(
-    section_list_object: SectionObject, item_id: int, index: int
+def insert_item_id_to_section_object(
+    *,
+    section_object: SectionObject,
+    item_id: int,
+    index: int,  # keyword arguments only
 ) -> SectionObject:
-    # convert string to list
-    section_list = StrListConverter.get_list_from_str(section_list_object.items)
-    # insert item_id at index
-    section_list[index:index] = [item_id]
-    # convert list to string
-    section_list_object.items = StrListConverter.make_str_from_list(section_list)
-    return section_list_object
+    item_list = StrListConverter.get_list_from_str(section_object.items)
+    item_list[index:index] = [item_id]
+    section_object.items = StrListConverter.make_str_from_list(item_list)
+    return section_object
 
 
-def remove_item_id_from_section_list_object(
-    section_list_object: SectionObject, item_id: int
+def remove_item_id_from_section_object(
+    *, section_object: SectionObject, item_id: int  # keyword arguments only
 ) -> SectionObject:
-    section_list = StrListConverter.get_list_from_str(section_list_object.items)
+    # this assumes there are no duplicates in sections string
+    section_list = StrListConverter.get_list_from_str(section_object.items)
     section_list.remove(item_id)
-    section_list_object.items = StrListConverter.make_str_from_list(section_list)
-    return section_list_object
+    section_object.items = StrListConverter.make_str_from_list(section_list)
+    return section_object
 
 
-def add_item_id_to_section_list_in_database(
-    section_list_id: int, item_id: int, index: int
+def insert_item_id_to_section_object_in_database(
+    *,  # keyword arguments only
+    session: Session,
+    section_id: int,
+    item_id: int,
+    index: int,
 ) -> None:
-    section_list_obj = select_section_list_obj_by_id(section_list_id)
+    section_object = select_section_object_by_id(session=session, section_id=section_id)
 
-    if section_list_obj is None:
-        raise SectionNotFoundExeption(section_list_id=str(section_list_id))
+    if section_object is None:
+        raise SectionNotFoundExeption(section_id=str(section_id))
     else:
-        section_list_obj = insert_item_id_to_section_list_object(
-            section_list_object=section_list_obj, item_id=item_id, index=index
-        )
-        update_section_list_object_in_database(section_list_object=section_list_obj)
-
-
-def remove_item_id_from_section_list_in_database(
-    section_list_id: int, item_id: int
-) -> None:
-    section_list_obj = select_section_list_obj_by_id(section_list_id)
-
-    if section_list_obj is None:
-        raise SectionNotFoundExeption(section_list_id=str(section_list_id))
-    else:
-        section_list_obj = remove_item_id_from_section_list_object(
-            section_list_obj, item_id
+        section_object = insert_item_id_to_section_object(
+            section_object=section_object, item_id=item_id, index=index
         )
 
-    update_section_list_object_in_database(section_list_obj)
+    update_section_object_in_database(session=session, section_object=section_object)
+
+
+def remove_item_id_from_section_object_in_database(
+    *, session: Session, section_id: int, item_id: int  # keyword arguments only
+) -> None:
+    section_object = select_section_object_by_id(session=session, section_id=section_id)
+
+    if section_object is None:
+        raise SectionNotFoundExeption(section_id=str(section_id))
+    else:
+        section_object = remove_item_id_from_section_object(
+            section_object=section_object, item_id=item_id
+        )
+
+    update_section_object_in_database(session=session, section_object=section_object)
