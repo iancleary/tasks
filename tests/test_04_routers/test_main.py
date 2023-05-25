e99f4b166cf0da5d479dadb632ecdadbeda63907
iancleary <github@iancleary.me>
2023-05-24 18:47:07 -0700

    #123: move response_model to router decorator, add docs and openapi.json tests

--- tests/test_04_routers/test_main.py
+++ tests/test_04_routers/test_main.py
@@ -5,6 +5,16 @@ from app.main import app
 client = TestClient(app)
 
 
+def test_openapi_json_check() -> None:
+    response = client.get("/openapi.json")
+    assert response.status_code == 200
+
+
+def test_docs_check() -> None:
+    response = client.get("/docs")
+    assert response.status_code == 200
+
+
 def test_health_check() -> None:
     response = client.get("/health")
     assert response.status_code == 200
